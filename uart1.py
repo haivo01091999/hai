7a0dfcf97c6186c51d67fdaa199c2234fede4187
haivo01091999 <47469581+haivo01091999@users.noreply.github.com>
2019-02-10 09:39:51 +0700

    Update uart1.py

--- uart1.py
+++ uart1.py
@@ -4,11 +4,11 @@ from time import sleep
 ser = serial.Serial('/dev/ttyACM0',9600) #mo cong serial voi baudrate 9600
 
 try:
-			while(true):
+			while(True):
 					if (ser.in_waiting >0):  #neu cos tin hieu tu arduino
 						data=ser.readline() #doc vao data
 						print(data)
-					else
+					else:
 						string = input('what do you send?')
 						string = string + "\r"
 						ser.write(string.encode()) #chuyen ve unicode va dua ra cho arrduino
