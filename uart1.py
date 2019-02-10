c1521dd1e938455a389cac4005c6e0471673c57c
haivo01091999 <47469581+haivo01091999@users.noreply.github.com>
2019-02-10 09:10:09 +0700

    Add files via upload

---
+++ uart1.py
@@ -0,0 +1,20 @@
+import serial  #khai bathu vien 
+from time import sleep
+
+ser = serial.Serial('/dev/ttyACM0',9600) #mo cong serial voi baudrate 9600
+
+try:
+			while(true):
+					if (ser.in_waiting >0)  #neu cos tin hieu tu arduino
+						data=ser.readline() #doc vao data
+						print(data)
+					else
+						string = input('what do you send?')
+
+						string = string + "/r"
+						ser.write(string.encode()) #chuyen ve unicode va dua ra cho arrduino
+						sleep(0.5)
+except KeyboardInterrupt:
+	print('done')
+finally:
+	ser.close() #dong cong serial
